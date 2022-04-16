c3f2121f1c2d65de77db9a0fbecd99785e9dad12
Felipe Miranda <felip.simi@gmail.com>
2022-04-15 23:40:41 -0300

    Migrations: Sequelize config

---
+++ config/sequelize/config.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+
+module.exports = {
+  local: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'postgres',
+    migrationStorageTableName: 'sequelize_meta',
+  },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'postgres',
+    migrationStorageTableName: 'sequelize_meta',
+  },
+};
