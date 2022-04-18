cb67e7362052f3657f91778e24ad27a0fbaa9efa
Felipe Miranda <felip.simi@gmail.com>
2022-04-17 23:57:34 -0300

    Events: database migration

---
+++ migrations/20220418053858-create-events.js
@@ -0,0 +1,63 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('events', {
+      id: {
+        type: Sequelize.UUID,
+        primaryKey: true,
+        allowNull: false,
+        field: 'id',
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        field: 'name',
+      },
+      description: {
+        type: Sequelize.STRING,
+        allowNull: true,
+        field: 'description',
+      },
+      eventDate: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        field: 'created_date',
+      },
+      isOutside: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        field: 'is_outside',
+      },
+      organizerId: {
+        type: Sequelize.UUID,
+        references: {
+          model: {
+            tableName: 'organizers',
+          },
+          key: 'id',
+        },
+        allowNull: false,
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        field: 'created_at',
+      },
+      deletedAt: {
+        type: Sequelize.DATE,
+        allowNull: true,
+        field: 'deleted_at',
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: true,
+        field: 'updated_at',
+      },
+    });
+  },
+
+  async down(queryInterface, _) {
+    await queryInterface.dropTable('events');
+  },
+};
