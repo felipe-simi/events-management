38ab5a7f6e3d914ef254a3ad44216ae8dee0c320
Felipe Miranda <felip.simi@gmail.com>
2022-04-24 23:49:38 -0300

    Fix: location DTO mapping

--- src/service/dto/LocationDto.ts
+++ src/service/dto/LocationDto.ts
@@ -37,27 +37,13 @@ export const mapLocationDtoToModel = (location: LocationDto): Location => {
 };
 
 export const mapLocationModelToDto = (location: Location): LocationDto => {
-  if (
-    location.latitudeIso !== undefined &&
-    location.longitudeIso !== undefined
-  ) {
-    return Location.fromGeographicCoordinates(
-      location.latitudeIso,
-      location.longitudeIso
-    );
-  }
-  if (
-    location.countryAlphaCode &&
-    location.cityName &&
-    location.streetAddress &&
-    location.postalCode
-  ) {
-    return Location.fromAddress(
-      location.countryAlphaCode,
-      location.cityName,
-      location.streetAddress,
-      location.postalCode
-    );
-  }
-  throw new Error('Invalid location!');
+  return {
+    id: location.id,
+    countryAlphaCode: location.countryAlphaCode,
+    cityName: location.cityName,
+    streetAddress: location.streetAddress,
+    postalCode: location.postalCode,
+    latitudeIso: location.latitudeIso,
+    longitudeIso: location.longitudeIso,
+  };
 };
